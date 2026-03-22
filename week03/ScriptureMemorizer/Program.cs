f7e501deed748ffbf0c5b8c9a683fb4796147b2b
justinbyu <justinriverodiaz@gmail.com>
2026-03-22 18:40:57 +0800

    finish

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,9 +1,135 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        Reference reference = new Reference("Proverbs", 3, 5, 6);
+        Scripture scripture = new Scripture(reference,
+            "Trust in the Lord with all thine heart and lean not unto thine own understanding");
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+
+            if (scripture.AllWordsHidden())
+            {
+                Console.WriteLine("\nAll words are hidden. Program ending.");
+                break;
+            }
+
+            Console.WriteLine("\nPress Enter to continue or type 'quit':");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+                break;
+
+            scripture.HideRandomWords(3);
+        }
+    }
+}
+
+class Reference
+{
+    private string _book;
+    private int _chapter;
+    private int _verseStart;
+    private int _verseEnd;
+
+    // Constructor for single verse
+    public Reference(string book, int chapter, int verse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verseStart = verse;
+        _verseEnd = verse;
+    }
+
+    // Constructor for verse range
+    public Reference(string book, int chapter, int verseStart, int verseEnd)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verseStart = verseStart;
+        _verseEnd = verseEnd;
+    }
+
+    public string GetDisplayText()
+    {
+        if (_verseStart == _verseEnd)
+            return $"{_book} {_chapter}:{_verseStart}";
+        else
+            return $"{_book} {_chapter}:{_verseStart}-{_verseEnd}";
+    }
+}
+
+class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+    private Random _random = new Random();
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = text.Split(' ')
+                     .Select(w => new Word(w))
+                     .ToList();
+    }
+
+    public void HideRandomWords(int count)
+    {
+        var visibleWords = _words.Where(w => !w.IsHidden()).ToList();
+
+        for (int i = 0; i < count && visibleWords.Count > 0; i++)
+        {
+            int index = _random.Next(visibleWords.Count);
+            visibleWords[index].Hide();
+            visibleWords.RemoveAt(index);
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string text = string.Join(" ", _words.Select(w => w.GetDisplayText()));
+        return $"{_reference.GetDisplayText()} {text}";
+    }
+
+    public bool AllWordsHidden()
+    {
+        return _words.All(w => w.IsHidden());
+    }
+}
+
+class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
+    public void Hide()
+    {
+        _isHidden = true;
+    }
+
+    public bool IsHidden()
+    {
+        return _isHidden;
+    }
+
+    public string GetDisplayText()
+    {
+        if (_isHidden)
+            return new string('_', _text.Length);
+        else
+            return _text;
     }
 }
